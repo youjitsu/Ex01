ced36603fb38e36eb273bef2b0e6393695243ba2
youjitsu <136162416+youjitsu@users.noreply.github.com>
2023-06-11 08:39:13 +0900

    Won/lost message modified

--- dice_game.c
+++ dice_game.c
@@ -23,9 +23,9 @@ int main() {
   total = dice1 + dice2;
   printf("Total value: %d\n", total);
   if (total > 7) {
-    printf("You won\n");
+    printf("%s won\n",name);
   } else {
-    printf("You lost\n");
+    printf("%s lost\n",name);
   }
 
   return 0;
