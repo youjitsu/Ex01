21f0b37652300b89a27c72a2574c482a7d44772a
youjitsu <136162416+youjitsu@users.noreply.github.com>
2023-06-11 08:04:24 +0900

    Dice game: initial revision

---
+++ dice_game.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int rollDice() {
+  return (rand() % 6) + 1;
+}
+
+int main() {
+  int dice1,dice2;
+  srand(time(0));  // Initialize random number generator                                                                
+  dice1=rollDice();
+  dice2=rollDice();
+  printf("Rolling dice...\n");
+  printf("Die 1: %d\n", dice1);
+  printf("Die 2: %d\n", dice2);
+  printf("Total value: %d\n", dice1+dice2);
+
+  return 0;
+
+}
